3899a98753ef480c4b1d990e8f30e16f37a91d67
Dharikasri <shridharika@gmail.com>
2024-05-17 15:04:46 +0530

    committed successfully

--- hello_flask.py
+++ hello_flask.py
@@ -2,10 +2,14 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
-@app.route("/login")
-def template():
-        return render_template("index.html")
+@app.route("/pageone")
+def one():
+    return render_template("firstpage.html")
+
+@app.route("/pagetwo")
+def two():
+    return render_template("secondpage.html")
 
 
 if __name__=="__main__":
-        app.run()
+    app.run()
