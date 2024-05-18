bed28ceb169adeb855aa29c43545751ab81314be
Dharikasri <shridharika@gmail.com>
2024-05-18 12:36:27 +0530

    committed successfully

--- hello_flask.py
+++ hello_flask.py
@@ -2,14 +2,15 @@ from flask import Flask,render_template,request,redirect
 
 app=Flask(__name__)
 
-@app.route("/inputpage")
+@app.route("/postinput")
 def input():
-    return render_template("inputpage.html")
+    return render_template("postinput.html")
 
-@app.route("/statuspage",methods=["GET"])
-def status():
-    status=request.args.get('textinput')
-    return render_template("statuspage.html",status=status)
+@app.route("/poststatus",methods=["POST"])
+def status(): 
+    status = request.form["textinput"]
+    print(status)
+    return render_template("poststatus.html",status=status)
     
 if __name__=="__main__":
     app.run()
